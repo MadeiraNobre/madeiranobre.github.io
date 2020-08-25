da8c05daf5609a0613a7a177d9d446d628667326
Takasakiii <lucasmc2709@live.com>
2020-08-25 16:00:26 -0300

    remove data

--- js/site.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    if(location.search){
-       infrate("paginas/movel");
-    }else{
-        infrate("paginas/home");
-    }
-
-    registerEvent(".navClick");
-});
