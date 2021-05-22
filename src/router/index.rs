1bd5321bb30fa138839d72b1625d9660956d1afb
Takasakiii <lucasmc2709@live.com>
2021-05-21 22:25:52 -0300

    first page

--- src/router/index.rs
+++ src/router/index.rs
@@ -10,7 +10,16 @@ impl Component for IndexPage{
 
     fn view(&self) -> Html {
         html! {
-            <div><h1>{"Ola"}</h1></div>
+            <div class="index-page__background">
+                <div class="index-page__form">
+                    <h1 class="index-page__form-title">{"Madeira Nobre Móveis"}</h1>
+                    <span class="index-page__form-span">{"Moveis • Arte • Decoração"}</span>
+                    <div class="index-page__form-buttons">
+                        <a class="index-page__form-button" href="/temporary_index.html">{"Nossos Produtos"}</a>
+                        <a class="index-page__form-button" href="/temporary_index.html#footer">{"Sobre a Loja"}</a>
+                    </div>
+                </div>
+            </div>
         }
     }
 
